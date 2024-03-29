4e7738a0d69d9bd9e6f4702c6e868fc911a64691
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2024-03-29 20:38:54 +0100

    Add Raycast script for opening notes

---
+++ .config/raycast/scripts/open-notes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Notes
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ✍🏻
+
+# Documentation:
+# @raycast.description Open notes in sublime
+
+# Path to the notes
+notes_path="$HOME/Documents/notes"
+
+# Find the last line number of the file
+last_line=$(awk 'END {print NR}' "$notes_path")
+
+# Open the file in Sublime Text, focusing on the last line
+subl "$notes_path":"$last_line"
+
