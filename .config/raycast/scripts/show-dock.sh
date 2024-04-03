9bd614c879323d0f8bf263072161f98966280f8f
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2024-04-03 23:40:49 +0200

    Fix warning message in raycast script
    killall can generate a "No matching processes belonging to you were found" message, which is now suppressed.

--- .config/raycast/scripts/show-dock.sh
+++ .config/raycast/scripts/show-dock.sh
@@ -14,11 +14,13 @@
 delay=$(defaults read com.apple.dock autohide-delay 2>/dev/null)
 
 if [ "$delay" -eq 1000 ] >/dev/null 2>&1 ; then
-    defaults write com.apple.dock autohide -bool false && killall Dock
-    defaults delete com.apple.dock autohide-delay && killall Dock
-    defaults write com.apple.dock no-bouncing -bool FALSE && killall Dock
+    defaults write com.apple.dock autohide -bool false
+    defaults delete com.apple.dock autohide-delay
+    defaults write com.apple.dock no-bouncing -bool FALSE
 else
-    defaults write com.apple.dock autohide -bool true && killall Dock
-    defaults write com.apple.dock autohide-delay -float 1000 && killall Dock
-    defaults write com.apple.dock no-bouncing -bool TRUE && killall Dock
+    defaults write com.apple.dock autohide -bool true
+    defaults write com.apple.dock autohide-delay -float 1000
+    defaults write com.apple.dock no-bouncing -bool TRUE
 fi
+# Restart Dock
+killall Dock >/dev/null 2>&1
